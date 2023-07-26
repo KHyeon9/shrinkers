a7b5a9df7ab2e35142fbf99074423c59116f0450
KHyeon9 <mrk19967@gmail.com>
2023-07-26 19:22:55 +0900

    class를 이용해 중복 부분 제거 및 user테이블 수정

--- shortener/models.py
+++ shortener/models.py
@@ -1,20 +1,26 @@
 import random
 import string
-from unicodedata import category
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User as U
 
 
 # Create your models here.
 
-class PayPlan(models.Model):
-    name = models.CharField(max_length=20)
-    price = models.IntegerField()
+class TimeStampedModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        abstract = True
 
-class Organization(models.Model):
+
+class PayPlan(TimeStampedModel):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+
+class Organization(TimeStampedModel):
     class Industries(models.TextChoices):
         PERSONAL = "personal"
         RETAIL = "retail"
@@ -27,35 +33,30 @@ class Organization(models.Model):
         max_length=15, choices=Industries.choices, default=Industries.OTHERS)
     pay_plan = models.ForeignKey(
         PayPlan, on_delete=models.DO_NOTHING, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Users(models.Model):
     user = models.OneToOneField(U, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100, null=True)
+    url_count = models.IntegerField(default=0)
     organization = models.ForeignKey(
         Organization, on_delete=models.DO_NOTHING, null=True)
 
 
-class EmailVerification(models.Model):
+class EmailVerification(TimeStampedModel):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     key = models.CharField(max_length=100, null=True)
     verified = models.BooleanField(default=False)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Categories(models.Model):
+class Categories(TimeStampedModel):
     name = models.CharField(max_length=100)
     organization = models.ForeignKey(
         Organization, on_delete=models.DO_NOTHING, null=True)
     creator = models.ForeignKey(Users, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class ShortenedUrls(models.Model):
+class ShortenedUrls(TimeStampedModel):
     class UrlCreatedVia(models.TextChoices):
         WEBSITE = "web"
         TELEGRAM = "telegram"
@@ -64,15 +65,17 @@ class ShortenedUrls(models.Model):
         str_pool = string.digits + string.ascii_letters
         return ("".join([random.choice(str_pool) for _ in range(6)])).lower()
 
+    def rand_letter():
+        str_pool = string.ascii_letters
+        return random.choice(str_pool).lower()
+
     nick_name = models.CharField(max_length=100)
     category = models.ForeignKey(
         Categories, on_delete=models.DO_NOTHING, null=True)
-    prefix = models.CharField(max_length=50)
+    prefix = models.CharField(max_length=50, default=rand_letter)
     creator = models.ForeignKey(Users, on_delete=models.CASCADE)
     target_url = models.CharField(max_length=2000)
     shortened_url = models.CharField(max_length=6, default=rand_string)
-    created_via = models.CharField(
+    create_via = models.CharField(
         max_length=8, choices=UrlCreatedVia.choices, default=UrlCreatedVia.WEBSITE)
     expired_at = models.DateTimeField(null=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
